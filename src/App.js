c603179d78345b4914a4d45b405c155faabe7259
Marcel Leandro <leantrs@gmail.com>
2021-11-08 17:49:55 -0300

    1 -

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Efetuando testes.
+          Edit <code>src/App.js</code> Marcel Leandro.
         </p>
         <a
           className="App-link"
